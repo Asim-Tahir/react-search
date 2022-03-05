e6f217414b452d43c368c7183ecfaa968f692242
Asım Tahir <a.karakus@pa.ag>
2022-03-05 13:43:52 +0300

    ref: move `Search` context provider into outside of `App` component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Aside, Nav, Main } from "@/views";
 import { Search } from "@/context";
 
 export default function App(): React.ReactElement {
+  const { isSearched } = useContext(Search.Context);
+
   return (
-    <Search.Provider>
-      <Aside />
+    <>
+      {isSearched && <Aside />}
       <div className="flex flex-col w-full h-full">
         <Nav />
         <Main />
       </div>
-    </Search.Provider>
+    </>
   );
 }
